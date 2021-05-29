1e6cddaf7f93686e41af58d9d25fa20edd214087
VWRoli <fustroli@gmail.com>
2021-05-29 10:15:27 +0200

    add auth token

--- guitar-practice-assistant/src/models/user.js
+++ guitar-practice-assistant/src/models/user.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
   },
@@ -31,6 +34,49 @@ const User = mongoose.model('User', {
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+//? GENERATE TOKEN
+userSchema.methods.generateAuthToken = async function () {
+  const token = jwt.sign(
+    { _id: this._id.toString() },
+    'guitarpracticeassistant'
+  );
+
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+  return token;
+};
+
+//? Find user, and match for password
+userSchema.statics.findByCredentials = async (username, password) => {
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new Error('Unable to login, please check your email and password!');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login, please check your email and password!');
+  }
+  return user;
+};
+
+//? Hash passwords before saving
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- guitar-practice-assistant/src/routers/user.js
+++ guitar-practice-assistant/src/routers/user.js
@@ -8,9 +8,24 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+//?LOGIN user
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.username,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send();
   }
 });
 
@@ -42,11 +57,21 @@ router.get('/users/:id', async (req, res) => {
 router.patch('/users/:id', async (req, res) => {
   const _id = req.params.id;
 
+  //Allowing certain keys to be updated
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates' });
+  }
+
   try {
-    const user = await User.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(_id);
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+
     if (!user) {
       return res.status(404).send();
     }
